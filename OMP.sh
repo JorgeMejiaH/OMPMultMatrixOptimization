8b27ff4c0b58c5ba90cc80a8a67238dc571b0c3e
Jorge Alejandro Mejia <j.mejia2@utp.edu.co>
2023-10-23 15:35:02 -0500

    Times

--- OMP.sh
+++ OMP.sh
@@ -4,8 +4,9 @@ echo "Size;Threads;Time" > MultMatrixOMP.csv
 for i in {1..10}; do
     echo "Running iteration $i"
     for t in 2 4 8 16 32; do
+        result=$(export OMP_NUM_THREADS=$t)
         for j in 32 64 128 256 512 1024 2048; do
-            result=$(./MultMatrixOMP $j $t)
+            result=$(./MultMatrixOMP $j)
             echo "$j;$t;$result" >> MultMatrixOMP.csv
         done
     done
